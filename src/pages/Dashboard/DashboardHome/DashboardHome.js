ac467aa1ef60a5f06b709a60d5acc900bea46129
rickywychoi <cchaff0105@gmail.com>
2021-02-12 13:06:33 -0800

    Set up DashboardHome component with Navbar

--- src/pages/Dashboard/DashboardHome/DashboardHome.js
+++ src/pages/Dashboard/DashboardHome/DashboardHome.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from './DashboardHome.module.css'
+import WithNavBar from '../../../hoc/WithNavbar'
 
 function DashboardHome() {
   return (
     <div className="container">
+      <h1>Dashboard Home</h1>
     </div>
   )
 }
 
-export default DashboardHome
+export default WithNavBar(DashboardHome)
