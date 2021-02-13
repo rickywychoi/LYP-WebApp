24efbb8090aff5530c7b7766c9b8e1e60fdcb3a0
rickywychoi <cchaff0105@gmail.com>
2021-02-13 10:42:28 -0800

    Changed folder structure

---
+++ src/hoc/WithNavbar/WithNavbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LYPNavbar from '../../components/LYPNavbar/LYPNavbar'
+
+function WithNavBar ( WrappingComponent ) {
+  return (props) => (
+    <div className="container">
+      <LYPNavbar />
+      <WrappingComponent {...props}/>
+    </div>
+  )
+}
+
+export default WithNavBar
