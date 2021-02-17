c66842edb47e5566180ee0c340a12725278be9d3
rickywychoi <cchaff0105@gmail.com>
2021-02-16 19:17:40 -0800

    Adjusted portion of three columns

--- src/hoc/LYPColumns/LYPColumns.js
+++ src/hoc/LYPColumns/LYPColumns.js
@@ -6,13 +6,13 @@ function LYPColumns (props) {
 
   return (
     <div className={`row ${styles.mainRow}`}>
-      <div className={`col-3 ${styles.side}`}>
+      <div className={`col-3 ${styles.sideLeft}`}>
         { React.cloneElement(children[0], {className: `sticky-top ${styles.sideWrapee}`}) }
       </div>
-      <div className='col-5'>
+      <div className='col-6'>
         { children[1] }
       </div>
-      <div className={`col-4 ${styles.side}`}>
+      <div className={`col-3 ${styles.sideRight}`}>
         { React.cloneElement(children[2], {className: `sticky-top ${styles.sideWrapee}`}) }
       </div>
     </div>
