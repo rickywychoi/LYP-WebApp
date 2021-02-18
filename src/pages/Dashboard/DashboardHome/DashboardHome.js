91041a2c6df28c897b0e7ee0adb45a337cc41ecc
rickywychoi <cchaff0105@gmail.com>
2021-02-18 08:53:27 -0800

    Added basic layout of newsfeed

--- src/pages/Dashboard/DashboardHome/DashboardHome.js
+++ src/pages/Dashboard/DashboardHome/DashboardHome.js
@@ -4,12 +4,13 @@ import LYPColumns from '../../../hoc/LYPColumns/LYPColumns'
 
 import SideWidget from '../../../containers/SideWidget/SideWidget'
 import AdSection from '../../../containers/AdSection/AdSection'
+import NewsFeed from '../../../containers/Dashboard/NewsFeed/NewsFeed'
 
 function DashboardHome () {
   return (
     <LYPColumns>
       <SideWidget />
-      <div style={{ backgroundColor: 'azure', height: '1500px' }}>2</div>
+      <NewsFeed />
       <AdSection />
     </LYPColumns>
   )
