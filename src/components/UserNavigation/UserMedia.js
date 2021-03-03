81d6243c87ee2fd8c98de87c1a93732a356b36d8
Gyeong-Min (Tyler) Lee <tyler7688@gmail.com>
2021-03-03 12:41:57 -0800

    tyler-03/03

--- src/components/UserNavigation/UserMedia.js
+++ src/components/UserNavigation/UserMedia.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React,  {useState} from 'react';
 import FriendDropDown from '../Search/FriendDropDown'
 import styles from './UserMedia.module.css'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
-import ToggleButton from 'react-bootstrap/ToggleButton'
+import RUG from 'react-upload-gallery'
+import ProfileCard from '../../components/ProfileCard/ProfileCard'
 
 
 function UserMedia() {
+
+    
+
     return(
         <div className={styles.container}>
 
             <div className={styles.empty_div}></div>
-            <div className={styles.form_container}>
+            <div className={styles.grid_container}>
                 <Form>
                     <Form.Group controlId="exampleForm.ControlTextarea1">
-                        <Form.Control as="textarea" rows={3} />
-                        <Button variant="outline-primary">Share</Button>
-                        <Form.File/>
+                        <div className={styles.textarea}>
+                            <Form.Control as="textarea" rows={3} />
+                        </div>
+                        <div className={styles.upload_file}>     
+                            <Button variant="outline-primary">Share</Button>
+                        </div>
+                        <div className={styles.share}>
+                        {/* <RUG
+                            action="/api/upload" // upload route
+                            source={response => response.source} // response image source
+                        /> */}
+                        </div>
+                        
                     </Form.Group>
                 </Form>
             </div>
             <div className={styles.empty_div}></div>
             <div className={styles.posts_container}>
-                photo gallery container
             </div>
         </div>
         
--- src/pages/User/UserProfile.js
+++ src/pages/User/UserProfile.js
@@ -1,18 +1,22 @@
 import React, { useState }  from 'react'
 import WithNavBar from '../../hoc/WithNavbar/WithNavbar'
-import Image from 'react-bootstrap/Image'
 import Avatar from 'react-avatar'
 import styles from './UserProfile.module.css'
 import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import classNames from 'classnames'
 import UserMedia from '../../components/UserNavigation/UserMedia'
 import UserFriends from '../../components/UserNavigation/UserFriends'
 import UserTimeline from '../../components/UserNavigation/UserTimeline'
 import UserAbout from '../../components/UserNavigation/UserAbout'
 import UserGroups from '../../components/UserNavigation/UserGroups'
+import ProfileCard from '../../components/ProfileCard/ProfileCard'
 
 function UserProfile() {
     const [nav, setNav] = useState("about");
+
     let navHandler;
+    
     const navigationTool = () => {
       if(nav === 'timeline'){
         navHandler = <UserTimeline/>
@@ -32,9 +36,27 @@ function UserProfile() {
     <>
     <div className={styles.sample}>
       <div className={styles.wrapper}>
-        <Avatar name="Tyler Lee" />
+
+        <div className={styles.grid_container}>
+          <div className={styles.avatar_container}>
+            <Avatar name="Tyler Lee" />
+          </div>
+
+          <div className={styles.name_container}>
+            <h1> Tyler Lee</h1>
+          </div>
+          <div className={styles.user_follower}>
+            followers
+          </div>
+        </div>
+
+        <div className={styles.grid_child}></div>
       </div>
+
+
       <br></br>
+
+
       <div className={styles.wrapper}>
         <Nav justify variant="tabs" defaultActiveKey="/home">
           <Nav.Item>
